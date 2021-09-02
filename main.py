3b411db82c73f8cb33ac98f234a67fd4bcb13b70
repo-son <lahiru980529@gmail.com>
2021-09-02 23:32:19 +0530

    Added Guessing Game to the App

--- main.py
+++
@@ -1,8 +0,0 @@
-name = input('Enter the First Name of the User: ')
-name_last = input('Enter the Second Name of the User: ')
-
-if name == 'Lahiru':
-    print(f'Full name Is: {name} {name_last}')
-else:
-    print('Go and Change your name')
-
